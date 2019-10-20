74c13752f61b5e04f05e9d34112a6f63b095dc16
Abhishek Agarwal <55159395+aagarwal937@users.noreply.github.com>
2019-10-20 18:35:11 +0530

    Add files via upload

--- tictactoegame8.py
+++ tictactoegame8.py
@@ -1,67 +1,58 @@
-print('Welcome to TIC TAC TOE')
+print('Welcome to TIC tAC TOE')
 
-while true:
-
+while True:
 	the_board = [' ']*10
-	player1_marker,player2_marker = player_input()
-
+	Player1_marker,Player2_marker = Player_input()
 	turn = choose_first
-	print(turn + 'will go first')
+	print(turn+'will go first')
 
-	play_game = input('Ready to play? y or n?')
+	play_game = input('ready to play? y or no')
 
-	if play_game == 'y':
+	if play_game == y:
 		game_on = True
-	
+
 	else:
-	    game_on = False	
+		game_on  = False
 
 	while game_on:
 
-		if turn == 'Player 1':
-
-		     display_board(the_board)
-
-		position = player_choice(the_board)
-		
-		place_marker(the_board,player1_marker,position)
-
-		if win_check(the_board,player1_marker):
+		if turn == 'Player1':
 			display_board(the_board)
-			print('Player1 has WON!!!')
-			game_on = False
+
+		position = Player_choice(the_board)
+
+		place_marker(the_board,Player1_marker,positon)
+
+		if win_check(the_board,Player1_marker):
+		    display_board(the_board)
+		    print('Player1 has WON!!!!!')
+		    game_on = False
 		
 		else:
-			
-			if full_board_check(the_board):
-			    display_board(the_board)
-			    print('TIE GAME!!!')
-			    game_on = False
-			else:
-			    turn = 'Player2'
 
-else:
-
-		display_board(the_board)
-
-		position = player_choice(the_board)
-		
-		place_marker(the_board,player2_marker,position)
-
-		if win_check(the_board,player2_marker):
+		    if full_board_check(the_board):
+		    	display_board(the_board)
+		    	print('TIE GAME!!!!')
+		    	game_on = False
+		    
+	else:
 			display_board(the_board)
-			print('Player2 has WON!!!')
-			game_on = False
 
-		else:
+	position = Player_choice(the_board)
 
-			if full_board_ckeck(the_board):
-			    display_board(the_board)
-			    print('TIE GAME!!!')
-			    game_on = False
-			
-			else:
-			    turn = 'Player1'
-	
-	if no_reply():
-		break	    
+	place_marker(the_board,Player1_marker,positon)
+
+	if win_check(the_board,Player1_marker):
+		 display_board(the_board)
+		 print('Player1 has WON!!!!!')
+		 game_on = False
+		
+	else:
+
+		if full_board_check(the_board):
+		    display_board(the_board)
+		    print('TIE GAME!!!!')
+		    game_on = False
+
+	if not replay():
+		break 	
